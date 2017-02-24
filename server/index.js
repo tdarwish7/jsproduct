82191930e02cd5631453f23cf22691226f7b1fb7
Tariq Darwish <darwishtariq21@gmail.com>
2017-02-24 16:43:36 -0500

    add body-parser, morgan, winston

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 //require babel require hook
+//babel compiles the javascript to meet browser requirements since our server is written in es6
 require('babel-core/register');
 //require server code
 require('./src');

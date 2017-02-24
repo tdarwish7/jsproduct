287e079726eeaaedaeaa32118df5a77744509f21
Tariq Darwish <darwishtariq21@gmail.com>
2017-02-24 17:26:02 -0500

    finished writing code for winston and morgan for logging instead of using console

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,15 @@
 // our packages
 import app from './app';
+import {logger} from './util';
 
 //start server
 app.listen(8080, function () {
   const host = this.address().address;
   const port = this.address().port;
-  console.log('Listening at http://${host}:${port}');
+  logger.info('Listening at http://${host}:${port}');
 });
 
 
 // output all uncaught exceptions
-process.on('uncaughtException', err => console.error('uncaught exception:', err));
-process.on('unhandledRejection', error => console.error('unhandled rejection:', error));
+process.on('uncaughtException', err => logger.error('uncaught exception:', err));
+process.on('unhandledRejection', error => logger.error('unhandled rejection:', error));

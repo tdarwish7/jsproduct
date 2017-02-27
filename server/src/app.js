53ff90b6f9635a4af2bd81a2831134fed436ff6a
Tariq Darwish <darwishtariq21@gmail.com>
2017-02-26 22:01:21 -0500

    installed babel-node and added a post request in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 
 // our packages
-import {logger} from './util'
+import {logger} from './util';
 
 // init app
 const app = express();
@@ -12,13 +12,23 @@ const app = express();
 // setup logging
 app.use(morgan('combined', {stream: logger.stream}));
 
-//add body parsing
+// add body parsing
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
 // test method
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// login method
+app.post('/login', (req, res) => {
+  const {username, password} = req.body;
+  if (username === 'test' && password === '123') {
+    res.send({username, id: 1});
+    return;
+  }
+  res.status(401).send({error: 'Incorrect username or password'});
 });
 
 // catch all unhandler errors
--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,14 @@
 import app from './app';
 import {logger} from './util';
 
-//start server
-app.listen(8080, function () {
+// start server
+app.listen(8080, function() {
   const host = this.address().address;
   const port = this.address().port;
   logger.info(`Listening at http://${host}:${port}`);
 });
 
 
-// output all uncaught exceptions
+// output all uncaught exceptions and rejections
 process.on('uncaughtException', err => logger.error('uncaught exception:', err));
 process.on('unhandledRejection', error => logger.error('unhandled rejection:', error));

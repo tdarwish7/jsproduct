73d4edac231b3eea95362c555c33e76f0136d23b
Tariq Darwish <darwishtariq21@gmail.com>
2017-03-04 17:39:57 -0500

    log errors during testing

--- server/src/util/logger.js
+++ server/src/util/logger.js
@@ -4,6 +4,15 @@ export const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
       level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+      level: do {
+        if(process.env.NODE_ENV === 'testing') {
+          'error';
+        } else if(process.env.NODE_ENV === 'production') {
+          'info';
+        } else {
+          'debug';
+        }
+      },
       colorize: true,
       timestamp: true,
       prettyPrint: true,
